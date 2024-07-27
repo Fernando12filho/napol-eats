7741c1c548bf0ac06ac3b00c25c429456acd3b61
Fernando Melo <fgmelofilho@icloud.com>
2024-07-27 13:15:00 -0400

    mobile and desktop versions ready

--- main.js
+++ main.js
@@ -1,25 +1,57 @@
 document.addEventListener("DOMContentLoaded", function() {
     const contactButton = document.getElementById("contact-whatsapp");
     const menuButton = document.getElementById("view-menu-whatsapp");
+    const orderNowButton = document.getElementById("order-now");
+    const orderNowMobileButton = document.getElementById("order-now-mobile");
 
     // Replace with your company's WhatsApp number
-    const whatsappNumber = "+1 (321) 310-6114"; 
+    const whatsappNumber = "+1(321)310-6114"; 
 
     // Messages to be sent
     const contactMessage = "Hello, I would like to contact you.";
     const menuMessage = "Hello, I would like to see the menu.";
+    const orderNowMessage = "Hello, I would like to place an order.";
+
+    function sendWhatsAppMessage(message) {
+        const url = `https://api.whatsapp.com/send?phone=${whatsappNumber}&text=${encodeURIComponent(message)}`;
+        window.open(url, "_blank");
+    }
 
     if (contactButton) {
         contactButton.addEventListener("click", function() {
-            const url = `https://api.whatsapp.com/send?phone=${whatsappNumber}&text=${encodeURIComponent(contactMessage)}`;
-            window.open(url, "_blank");
+            sendWhatsAppMessage(contactMessage);
         });
     }
 
     if (menuButton) {
         menuButton.addEventListener("click", function() {
-            const url = `https://api.whatsapp.com/send?phone=${whatsappNumber}&text=${encodeURIComponent(menuMessage)}`;
-            window.open(url, "_blank");
+            sendWhatsAppMessage(menuMessage);
         });
     }
+
+    if (orderNowButton) {
+        orderNowButton.addEventListener("click", function() {
+            sendWhatsAppMessage(orderNowMessage);
+        });
+    }
+
+    if (orderNowMobileButton) {
+        orderNowMobileButton.addEventListener("click", function() {
+            sendWhatsAppMessage(orderNowMessage);
+        });
+    }
+
+    // Toggle mobile nav
+    const menuToggle = document.getElementById("menu-toggle");
+    const mobileNav = document.getElementById("mobile-nav");
+    const closingMobileNav = document.getElementById("closing-button")
+
+    menuToggle.addEventListener("click", function() {
+        mobileNav.style.display = 'block';
+    });
+    
+    closingMobileNav.addEventListener("click", function(){
+        mobileNav.style.display = 'none';
+    })
+
 });
